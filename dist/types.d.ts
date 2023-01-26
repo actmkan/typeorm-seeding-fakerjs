2b50a33ca0765346eaddd270807e46780f7e6845
mkan <actmkan@gmail.com>
2023-01-26 18:35:55 +0900

    change faker

---
+++ dist/types.d.ts
@@ -0,0 +1,35 @@
+import { Connection, ObjectType } from 'typeorm';
+import { Faker as FakerType } from '@faker-js/faker';
+import { EntityFactory } from './entity-factory';
+export type Faker = FakerType;
+/**
+ * FactoryFunction is the fucntion, which generate a new filled entity
+ */
+export type FactoryFunction<Entity, Context> = (faker: Faker, context?: Context) => Entity;
+/**
+ * EntityProperty defines an object whose keys and values must be properties of the given Entity.
+ */
+export type EntityProperty<Entity> = {
+    [Property in keyof Entity]?: Entity[Property];
+};
+/**
+ * Factory gets the EntityFactory to the given Entity and pass the context along
+ */
+export type Factory = <Entity, Context>(entity: ObjectType<Entity>) => (context?: Context) => EntityFactory<Entity, Context>;
+/**
+ * Seed are the class to create some data. Those seed are run by the cli.
+ */
+export interface Seeder {
+    run(factory: Factory, connection: Connection): Promise<void>;
+}
+/**
+ * Constructor of the seed class
+ */
+export type SeederConstructor = new () => Seeder;
+/**
+ * Value of our EntityFactory state
+ */
+export interface EntityFactoryDefinition<Entity, Context> {
+    entity: ObjectType<Entity>;
+    factory: FactoryFunction<Entity, Context>;
+}
